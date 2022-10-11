ec84d2bd4b19c7ca67c1dc495ce57f42ec19417d
jalexss <ja.soledads@gmail.com>
2022-10-11 18:28:47 -0400

    cambios en el navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,41 +1,47 @@
 import { FC } from "react"
 import NextLink from "next/link"
-import { AppBar, Box, Link, Toolbar, Typography } from "@mui/material"
+import { AppBar, Box, Grid, Link, Toolbar, Typography } from "@mui/material"
 
 export const Navbar:FC = () => {
   return (
     <AppBar position="static" color="secondary" >
 
-      <Toolbar >
-        <img 
-          alt="Icon de League of Legends" 
-          src="/img/IconLol.svg" 
-          style={{ width: '46px', height: '46px' }} 
-        />
-        
-        <Box 
-          display="flex" 
-          flexDirection="row"
-          sx={{ px: 2 }} 
+      <Toolbar>
+        <Grid 
+          container  
+          justifyContent='flex-start'
+          alignItems="center"
         >
-          <NextLink href="/" passHref>
-            <Link underline="hover" >
-              <Typography variant="body1" component="p" sx={{ mr: 2 }} >
-                Home
-              </Typography>
-            </Link>
-          </NextLink>
+          <img 
+            alt="Icon de League of Legends" 
+            src="/img/IconLol.svg" 
+            style={{ width: '46px', height: '46px' }} 
+          />
+          
+          <Box 
+            display="flex" 
+            flexDirection="row"
+            sx={{ px: 2 }} 
+          >
+            <NextLink href="/" passHref>
+              <Link underline="hover" >
+                <Typography variant="body1" component="p" sx={{ mr: 2 }} >
+                  Home
+                </Typography>
+              </Link>
+            </NextLink>
 
 
-          <NextLink href="/champion" passHref>
-            <Link underline="hover" >
-              <Typography color="inherit" variant="body1" component="p">
-                Campeones
-              </Typography>
-            </Link>
-          </NextLink>
-        </Box>
-        <Box flex={1} />
+            <NextLink href="/champion" passHref>
+              <Link underline="hover" >
+                <Typography color="inherit" variant="body1" component="p">
+                  Campeones
+                </Typography>
+              </Link>
+            </NextLink>
+          </Box>
+        </Grid>
+        
       </Toolbar>
     </AppBar>
   )
