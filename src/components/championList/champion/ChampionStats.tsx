77ba8d35798eb2bc42c7639cc14257d77b73a627
jalexss <ja.soledads@gmail.com>
2022-10-28 13:00:57 -0400

    agregando key en championStats

--- src/components/championList/champion/ChampionStats.tsx
+++ src/components/championList/champion/ChampionStats.tsx
@@ -29,8 +29,8 @@ export const ChampionStats:FC<TProps> = ({champion}) => {
       >
         <Typography color="primary" component="h1" variant="h4" >Estadísticas</Typography>
         {
-          stadistics.map( stats => (
-            <Stack direction="row" alignItems="center" spacing={1} sx={{ my: 1 }} >
+          stadistics.map( (stats, index) => (
+            <Stack key={index} direction="row" alignItems="center" spacing={1} sx={{ my: 1 }} >
               <Grid width={10} height={10} sx={{ backgroundColor: 'error.main', borderRadius: '10px' }} />
               <Typography align="justify" color="lightgreen" component="p" variant="body1" >{stats[0]}:</Typography>
               <Typography align="justify" component="p" variant="body1" >{stats[1]}:</Typography>
